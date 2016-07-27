74d4b378e80028b99cc36959ccf01f0225992b08
zhouxi <15651342081@163.com>
2016-07-27 21:56:41 +0800

    JS进阶技能
    http://www.imooc.com/learn/59

---
+++ perfect movement_copy.js
@@ -0,0 +1,18 @@
+
+
+//get css style
+function getStyle(obj, attr) {
+	if(obj.currentStyle){
+		return obj.currentStyle[attr];
+	}
+	esle
+	    return getComputedStyle(obj,false)[attr];
+}
+
+
+function startMove(){
+	
+	1 //get current style
+	
+	2 //
+}
